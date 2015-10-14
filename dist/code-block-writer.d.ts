cf68a61d49cdf5e63c86344fbe18ad98a44dac4c
David Sherret <dsherret@gmail.com>
2015-10-14 01:04:42 -0400

    Fixed up declaration file and fixed usage with tsd link

--- dist/code-block-writer.d.ts
+++ dist/code-block-writer.d.ts
@@ -1,13 +1,16 @@
-export default class CodeBlockWriter {
-    private _currentIndentation;
-    private _text;
-    private _numberSpaces;
-    private _lastWasNewLine;
-    block(block: () => void): CodeBlockWriter;
-    writeLine(str: string): CodeBlockWriter;
-    newLineIfLastNotNewLine(): CodeBlockWriter;
-    newLine(): CodeBlockWriter;
-    write(str: string): CodeBlockWriter;
-    toString(): string;
-    private _getCurrentIndentationNumberSpaces();
+declare module "code-block-writer" {
+    class CodeBlockWriter {
+        private _currentIndentation;
+        private _text;
+        private _numberSpaces;
+        private _lastWasNewLine;
+        block(block: () => void): CodeBlockWriter;
+        writeLine(str: string): CodeBlockWriter;
+        newLineIfLastNotNewLine(): CodeBlockWriter;
+        newLine(): CodeBlockWriter;
+        write(str: string): CodeBlockWriter;
+        toString(): string;
+        private _getCurrentIndentationNumberSpaces();
+    }
+    export default CodeBlockWriter;
 }
