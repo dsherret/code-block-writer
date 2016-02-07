be86baac5916f15428252200cdb390a2c55694b8
David Sherret <dsherret@gmail.com>
2016-02-06 20:10:07 -0500

    Fix definition file

--- code-block-writer.d.ts
+++ code-block-writer.d.ts
@@ -1,19 +1,20 @@
+declare class CodeBlockWriter {
+    private _currentIndentation;
+    private _text;
+    private _numberSpaces;
+    private _lastWasNewLine;
+    constructor(opts?: { newLine: string });
+    block(block: () => void): CodeBlockWriter;
+    getLength(): number;
+    writeLine(str: string): CodeBlockWriter;
+    newLineIfLastCharNotNewLine(): CodeBlockWriter;
+    newLine(): CodeBlockWriter;
+    spaceIfLastNotSpace(): CodeBlockWriter;
+    write(str: string): CodeBlockWriter;
+    toString(): string;
+    private _getCurrentIndentationNumberSpaces();
+}
+
 declare module "code-block-writer" {
-    class CodeBlockWriter {
-        private _currentIndentation;
-        private _text;
-        private _numberSpaces;
-        private _lastWasNewLine;
-        constructor(opts?: { newLine: string });
-        block(block: () => void): CodeBlockWriter;
-        getLength(): number;
-        writeLine(str: string): CodeBlockWriter;
-        newLineIfLastCharNotNewLine(): CodeBlockWriter;
-        newLine(): CodeBlockWriter;
-        spaceIfLastNotSpace(): CodeBlockWriter;
-        write(str: string): CodeBlockWriter;
-        toString(): string;
-        private _getCurrentIndentationNumberSpaces();
-    }
-    export default CodeBlockWriter;
+    export = CodeBlockWriter;
 }
