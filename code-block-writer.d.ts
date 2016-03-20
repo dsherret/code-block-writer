3a0f1cfc64fb5d4672180bfd6c0046120c99ad07
David Sherret <dsherret@gmail.com>
2016-03-20 12:41:47 -0400

    2.0.5 - Removed private members from definition file

--- code-block-writer.d.ts
+++ code-block-writer.d.ts
@@ -1,8 +1,4 @@
 export default class CodeBlockWriter {
-    private _currentIndentation;
-    private _text;
-    private _numberSpaces;
-    private _lastWasNewLine;
     constructor(opts?: { newLine: string });
     block(block: () => void): CodeBlockWriter;
     getLength(): number;
@@ -12,5 +8,4 @@ export default class CodeBlockWriter {
     spaceIfLastNotSpace(): CodeBlockWriter;
     write(str: string): CodeBlockWriter;
     toString(): string;
-    private _getCurrentIndentationNumberSpaces();
 }
