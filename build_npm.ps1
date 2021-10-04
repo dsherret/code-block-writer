24dd0bf211b8f660420b204bb66a9a0095c44f85
David Sherret <dsherret@gmail.com>
2021-10-04 17:55:59 -0400

    chore: switch to using dnt for creating an npm distrubtion

--- build_npm.ps1
+++ build_npm.ps1
@@ -1,1 +1,15 @@
-deno run --allow-read=./ --allow-write=./npm --unstable scripts/build_npm.ts
+$ErrorActionPreference = "Stop"
+
+deno run --allow-read=./ --allow-write=./npm --allow-net --no-check https://deno.land/x/dnt@0.0.3/cli.ts `
+   -- mod.ts `
+   --typeCheck `
+   --declaration `
+   --outDir ./npm/dist `
+   --removeComments `
+   --esModuleInterop `
+   --stripInternal `
+   --target ES2015 `
+   --module commonjs
+
+# include license
+copy LICENSE ./npm/LICENSE
