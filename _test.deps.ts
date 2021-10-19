597fb0c00b37b13728270c47329f885bf35a2390
David Sherret <dsherret@gmail.com>
2021-10-19 14:33:30 -0400

    Using mocha pollyfill for tests with dnt.

---
+++ _test.deps.ts
@@ -0,0 +1,2 @@
+export { expect } from "https://cdn.skypack.dev/chai@4.3.4?dts";
+import "https://gist.githubusercontent.com/lucacasonato/54c03bb267074aaa9b32415dbfb25522/raw/5016dc4c1ac585693e7f8756932f585d00af1b8b/deno_mocha.ts";
