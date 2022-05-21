267e7fed66d55ee5838878d9ddf7019db37174ba
David Sherret <dsherret@gmail.com>
2022-05-21 13:29:35 -0400

    chore: update test dependencies

--- _test.deps.ts
+++ _test.deps.ts
@@ -1,6 +1,5 @@
-// temporary until deno_std 0.140 is released
-export { describe, it } from "https://raw.githubusercontent.com/denoland/deno_std/dfbb3f9e21e57f71c0ceafb0dffe4692029d2602/testing/bdd.ts";
-import { assertEquals, assertThrows } from "https://raw.githubusercontent.com/denoland/deno_std/dfbb3f9e21e57f71c0ceafb0dffe4692029d2602/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.140.0/testing/bdd.ts";
+import { assertEquals, assertThrows } from "https://deno.land/std@0.140.0/testing/asserts.ts";
 
 // temporary until we get this in std
 export function expect<T>(value: T) {
