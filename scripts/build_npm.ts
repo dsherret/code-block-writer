30b6a290a147cf1f1112a344a7f40a52e18b50a5
David Sherret <dsherret@gmail.com>
2023-03-24 21:23:46 -0400

    fix: switch to `export =` in cjs output

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,7 +1,9 @@
+import $ from "https://deno.land/x/dax@0.30.1/mod.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
 
 await emptyDir("./npm");
 
+$.logStep("Building npm package...");
 await build({
   entryPoints: ["mod.ts"],
   typeCheck: true,
@@ -39,3 +41,21 @@ await build({
 
 Deno.copyFileSync("LICENSE", "npm/LICENSE");
 Deno.copyFileSync("README.md", "npm/README.md");
+
+// todo: this is temporary in order to get a regular `export =`
+$.logStep("Updating output to use export = ...");
+const npmDir = $.path(import.meta).parentOrThrow().parentOrThrow().join("npm");
+const jsFile = npmDir.join("script/mod.js");
+let jsFileText = jsFile.readTextSync();
+jsFileText = jsFileText.replace(`Object.defineProperty(exports, "__esModule", { value: true });`, "");
+jsFileText = jsFileText.replace(`exports.default = CodeBlockWriter;`, "module.exports = CodeBlockWriter;");
+jsFile.writeTextSync(jsFileText);
+
+const declFile = npmDir.join("types/mod.d.ts");
+let declText = declFile.readTextSync();
+declText = declText.replace("export default class ", "declare class ");
+declText = declText + "\nexport = CodeBlockWriter";
+
+$.logStep("Rerunning tests...");
+await $`npm run test`.cwd(npmDir);
+$.logStep("Complete!");
