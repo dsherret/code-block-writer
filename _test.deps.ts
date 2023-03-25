f578e0fa3416721dd68efaf5f85c52246ed499b1
David Sherret <dsherret@gmail.com>
2023-03-24 21:09:40 -0400

    chore: update test deps

--- _test.deps.ts
+++ _test.deps.ts
@@ -1,26 +1,3 @@
-export { describe, it } from "https://deno.land/std@0.140.0/testing/bdd.ts";
-import { assertEquals, assertThrows } from "https://deno.land/std@0.140.0/testing/asserts.ts";
-
-// temporary until we get this in std
-export function expect<T>(value: T) {
-  return {
-    to: {
-      deep: {
-        equal(otherValue: T) {
-          assertEquals(value, otherValue);
-        },
-      },
-      equal(otherValue: T, message?: string) {
-        assertEquals(value, otherValue, message);
-      },
-      throw(message?: string) {
-        assertThrows(value as any, undefined, undefined, message);
-      },
-      not: {
-        throw() {
-          (value as any)();
-        },
-      },
-    },
-  };
-}
+export { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
+// @deno-types="npm:@types/chai@4.3"
+export { expect } from "npm:chai@4.3.7";
