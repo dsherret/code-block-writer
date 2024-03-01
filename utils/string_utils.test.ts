408d2a086d795207b1967eace782d5f08159e0c0
David Sherret <dsherret@users.noreply.github.com>
2024-03-01 13:37:28 -0500

    fix: handle escapes better in .quote() (#46)

--- utils/string_utils.test.ts
+++ utils/string_utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "../_test.deps.ts";
-import { escapeChar, escapeForWithinString, getStringFromStrOrFunc } from "./string_utils.ts";
+import { escapeForWithinString, getStringFromStrOrFunc } from "./string_utils.ts";
 
 describe("escapeForWithinString", () => {
   function doTest(input: string, expected: string) {
@@ -9,27 +9,21 @@ describe("escapeForWithinString", () => {
   it("should escape the quotes and newline", () => {
     doTest(`"testing\n this out"`, `\\"testing\\\n this out\\"`);
   });
-});
 
-describe("escapeChar", () => {
-  function doTest(input: string, char: string, expected: string) {
-    expect(escapeChar(input, char)).to.equal(expected);
+  function doQuoteTest(input: string, quote: string, expected: string) {
+    expect(escapeForWithinString(input, quote)).to.equal(expected);
   }
 
-  it("should throw when specifying a char length > 1", () => {
-    expect(() => escapeChar("", "ab")).to.throw();
-  });
-
-  it("should throw when specifying a char length < 1", () => {
-    expect(() => escapeChar("", "")).to.throw();
-  });
-
   it("should escape the single quotes when specified", () => {
-    doTest(`'testing "this" out'`, `'`, `\\'testing "this" out\\'`);
+    doQuoteTest(`'testing "this" out'`, `'`, `\\'testing "this" out\\'`);
   });
 
   it("should escape regardless of if the character is already escaped", () => {
-    doTest(`"testing \\"this\\" out"`, `"`, `\\"testing \\\\"this\\\\" out\\"`);
+    doQuoteTest(`"testing \\"this\\" out"`, `"`, `\\"testing \\\\\\"this\\\\\\" out\\"`);
+  });
+
+  it("should escape unicode escape sequences", () => {
+    doQuoteTest(`\\u0009`, `"`, `\\\\u0009`);
   });
 });
 
